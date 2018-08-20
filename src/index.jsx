e01f2b6169076b825bfa845b92e0873eb0052d00
Ilia Isupov <matumbaman@yandex-team.ru>
2018-08-20 14:45:50 +0300

    Added redux persist. State persisting between sessions.

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,25 @@ import {createStore} from 'redux';
 import rootReducer from './reducers';
 import App from './components/App.jsx';
 
-const store = createStore(rootReducer);
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {PersistGate} from 'redux-persist/integration/react';
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer)
+const persistor = persistStore(store)
 
 render(
     <Provider store={store}>
-        <App/>
+        <PersistGate persistor={persistor}>
+            <App/>
+        </PersistGate>
     </Provider>,
     document.getElementById('app')
 );
